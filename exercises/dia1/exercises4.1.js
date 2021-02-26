a130802edcf4fa46484921c90365ae8c91671485
Lucas <lucasml@live.com>
2021-02-26 18:25:50 -0300

    Exercicio Pratico 1

---
+++ exercises/dia1/exercises4.1.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 2;
+
+soma = a+b;
+subtracao = a-b;
+multiplicacao = a*b;
+divisao = a/b;
+modulo = a**b;
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
