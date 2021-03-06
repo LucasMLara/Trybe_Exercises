300c676c3ac5d63225aeb539718baebb52efa8d6
Lucas <lucasml@live.com>
2021-03-06 13:54:24 -0300

    Exercicios 3 (outra maneira)

--- exercises4.2.js
+++ exercises4.2.js
@@ -1,7 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let sum = 0;
-for(index=0; index < numbers.length; index += 1) {
-  sum+=numbers[index];
+let media = 0;
+for(let index=0; index < numbers.length; index += 1) {
+  media+=numbers[index]/numbers.length
+
 }
-console.log(sum);
+//media=media/numbers.length;
+
+console.log(media);
