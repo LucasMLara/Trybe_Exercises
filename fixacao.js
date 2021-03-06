c2050c28f7c676e892a3db3be9b21a00014d6981
Lucas <lucasml@live.com>
2021-03-06 08:14:14 -0300

    exercicio js materia for

--- fixacao.js
+++ fixacao.js
@@ -12,3 +12,11 @@ console.log(menuServices);
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 menu.push('Contato');
 console.log(menu);
+
+//_______________________________________________________________________________________________
+
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+for (index = 0; index < groceryList.length; index +=1) {
+  console.log(groceryList[index])
+}
