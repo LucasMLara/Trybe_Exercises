e6831cfdd95c61a798fd0e556cdd7fc211f82897
Lucas <lucasml@live.com>
2021-03-07 10:41:14 -0300

    Exercicio 6

--- exercises4.2.js
+++ exercises4.2.js
@@ -1,12 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let oddList =0;
 
-let media = 0;
 for(let index=0; index < numbers.length; index += 1) {
-  media+=numbers[index]/numbers.length
+  if (numbers[index] % 2 !== 0) {
+    oddList+=1;
+  }
+ }
 
-}
-if (media>20) {
-  console.log('Valor maior que 20')
-} else {
-  console.log('Valor menor que 20')
-}
+ if (oddList === 0) {
+   console.log('Nenhum valor Ímpar encontrado');
+ } else {
+   console.log(oddList);
+ }
