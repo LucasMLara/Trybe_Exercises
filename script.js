e20b2af47cb3a1273a211c578be2ff24fbe6c86f
Lucas <lucasml@live.com>
2021-03-13 14:09:00 -0300

    realocando documentos na mesma pasta

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('h1')[0].style.color = "white";
+
+document.querySelectorAll('h1')[0].style.textAlign = "center";
+
+document.querySelectorAll('h1')[0].style.backgroundColor = "#2fc18c";
