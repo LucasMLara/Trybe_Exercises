9d229c0834932b1462025b8981e61bd81715ee1e
LucasMLara <lucasml@live.com>
2021-03-28 18:38:09 -0300

    Padronizando Branch nova

--- exercises/5.1/acompanhamentoAula/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('h1')[0].style.color = "white";
-
-document.querySelectorAll('h1')[0].style.textAlign = "center";
-
-document.querySelectorAll('h1')[0].style.backgroundColor = "#2fc18c";
