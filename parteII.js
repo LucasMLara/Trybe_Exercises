d185ef38d064ad8442429e81337c4bb8720ff8dc
LucasMLara <lucasml@live.com>
2021-03-31 18:42:21 -0300

    Parte II - 2

--- parteII.js
+++ parteII.js
@@ -1,11 +1,26 @@
-const descendMultiply = (number) => {
-  let result;
-  if (number > 0) {
-    result = 1;
-    for (let index = number; index > 1; index -= 1) {
-      result = result * index;
-      console.log(result );
-    }
-  }
-};
-descendMultiply(5);
+// const descendMultiply = (number) => {
+//   let result;
+//   if (number > 0) {
+//     result = 1;
+//     for (let index = number; index > 1; index -= 1) {
+//       result = result * index;
+//       console.log(result);
+//     }
+//   }
+// };
+// descendMultiply(5);
+
+
+const  longestWord = (phrase) => {
+  
+  let strInArray = phrase.split(' ')
+
+  const sortedArray = strInArray.sort((a, b) => b.length-a.length)
+  
+  const biggestWord = sortedArray[0];
+  console.log(biggestWord);
+  
+
+}
+
+longestWord('Tiago Santos e Lucas Muniz Lara estudando feito Nabucodonosor')
