e43e9e1da9fd8c5309d1b53d8245f30e211755ba
LucasMLara <lucasml@live.com>
2021-04-01 14:53:09 -0300

    primeiro exemplo - criando funcao que cria objeto

---
+++ acompanhamento.js
@@ -0,0 +1,73 @@
+const addOrSetParamInObject = (obj, key, value) => {
+
+  
+    obj[key] = value;
+    
+    return obj;
+}
+
+let trybeStudent = {};
+console.log(addOrSetParamInObject(trybeStudent, 'Nome', 'Lucas'));
+
+// COMO CRIAR O NOME DO OBJETO DE FORMA DINÂMICA NA FUNÇÃO?
+
+// let wowPlayer = addOrSetParamInObject({}, 'Scream', 'leroooooooy Jeeeeeeenkins');
+// console.log(wowPlayer);
+// wowPlayer = addOrSetParamInObject(wowPlayer, 'Newbie', 'Lara');
+// // wowPlayer.newbie = 'Lucas Lara'
+// console.log(wowPlayer);
+
+/* 
+WowPlayer = {
+  id: leroooooooy
+}
+
+ */
+
+
+
+// const xablau = {
+//   id: '1',
+//   idade: 45,
+//   profissao: 'developer'
+// }
+
+// const id = 'profissao';
+// const obj = {
+//   setor: 'financeiro',
+//   horario: 'noite'
+// }
+
+
+
+
+// mkObj(, ISHINUA, 'PEDROSO');
+
+
+// obj[key] = value
+//   return obj;
+// }
+
+
+// const obj = mkObj({}, 'nome', 'Lucas');
+// console.log(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // obj[key] = value;
+  // return obj;
+
+// mkObj({id:1}, 'Nome', 'Lucas');
+
+// console.log(mkObj({id:1}, 'Nome', 'Lucas'));
