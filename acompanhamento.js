7d128b114af07d5a4759214754befe536197cf12
LucasMunizLara <lucasml@live.com>
2021-04-16 14:11:43 -0300

    exemplo 1 9.1

--- acompanhamento.js
+++ acompanhamento.js
@@ -1,11 +1,16 @@
 const assert = require('assert');
 
-const pushNumber = (list, number) => list.push(number);
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-let numbers = [];
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
+};
 
-setTimeout(() => pushNumber(numbers, 1), 3000);
-pushNumber(numbers, 2);
-pushNumber(numbers, 3);
-
-assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
