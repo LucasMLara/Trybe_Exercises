88f75cb1956ea555a55b11065cb5b0745bd72c19
LucasMLara <lucasml@live.com>
2021-04-19 19:50:45 -0300

    arquivo inicial plantao zeze bloco 9

--- acompanhamento.js
+++ acompanhamento.js
@@ -13,7 +13,6 @@ const myExpenses = [
   },
 ];
 
-<<<<<<< HEAD
 const myIncome = 1000;
 
 const monthlyBudget = (myIncome, myExpenses, callback) => {
@@ -34,19 +33,3 @@ const handleExpenses = myExpenses => {
 };
 
 monthlyBudget(myIncome, myExpenses, handleExpenses);
-=======
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
-
-const getUser = (callback) => {
-  const userToReturn = {
-    firstName: "Ivan",
-    lastName: "Ivanovich",
-    nationality: "Russian"
-  };
-  return callback(userToReturn);
-};
-
-assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
-assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
->>>>>>> 7d128b114af07d5a4759214754befe536197cf12
