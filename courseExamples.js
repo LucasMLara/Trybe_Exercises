1c2a661572249fbba6a303645846e1a0e2ae1761
Lucas Lara <lucasml@live.com>
2021-08-17 23:25:05 -0300

    Logical Operators

--- courseExamples.js
+++ courseExamples.js
@@ -18,3 +18,33 @@ db.restaurants.count({rating: {$lt:4}});
 use ('business');
 
 db.restaurants.count({rating: {$nin:[5,6,7]}});
+
+// Selecione e faça a contagem dos restaurantes que não possuem avaliação menor ou igual a 5 , essa consulta também deve retornar restaurantes que não possuem o campo avaliação.
+use ('business');
+
+db.restaurants.count({rating: {$not: {$lte:5}}});
+// Selecione e faça a contagem dos restaurantes em que a avaliação seja maior ou igual a 6 , ou restaurantes localizados no bairro Brooklyn .
+use ('business');
+
+db.restaurants.count( { $or: [ { rating: {$gte: 6} },{ borough:{$eq: "Brooklyn"} } ] } );
+
+db.restaurants.count( { $or: [ { rating: {$gte: 6} },{ borough:"Brooklyn"} ] } );
+
+// Selecione e faça a contagem dos restaurantes localizados nos bairros Queens , Staten Island e Broklyn e possuem avaliação maior que 4 .
+use ('business');
+
+db.restaurants.count(
+  { $and:[ 
+      {borough: {$in:['Queens', 'Staten Island', 'Brooklyn']}},
+      {rating: {$gt: 4} }  ]
+  }
+);
+
+// Selecione e faça a contagem dos restaurantes onde nem o campo avaliação seja igual a 1 , nem o campo culinária seja do tipo American .
+use ('business');
+
+db.restaurants.count({$nor:[{rating:1},{cuisine:'American'}]});
+
+// Selecione e faça a contagem dos resturantes em que a avaliação seja maior que 6 ou menor que 10 , 
+// E esteja localizado no bairro Brooklyn , OU não possuem culinária do tipo Delicatessen .
+
