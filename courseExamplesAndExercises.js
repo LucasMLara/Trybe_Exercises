b55fc523e23f6584067ba604a5fbf1c2d5dbc8ff
Lucas Lara <lucasml@live.com>
2021-08-22 13:45:55 -0300

    Bonus Done

--- courseExamplesAndExercises.js
+++ courseExamplesAndExercises.js
@@ -150,6 +150,77 @@ db.movies.updateMany(
 
 // Exercício 14 : Remova o campo class dos documentos que possuem esse campo com o valor unknown .
 use('class')
+db.xmen.find();
+
+use('class')
+
+db.xmen.updateMany(
+  {class: "unknown"},
+    { $unset: {
+        class:""
+    },
+    $currentDate: {
+      lastUpdate: {$type: 'timestamp'}
+    }
+    }
+  );
+
+// Exercício 15 : Produza uma query que renomeie os campos de name para hero_name , e de true_name para full_name ; adicione o campo power com valor 100, em todos os documentos.
+
+use('class')
+db.xmen.updateMany({}, {$rename: {
+  name: 'hero_name', true_name:'full_name'
+}, $set:{
+  power:100
+}, $currentDate:{ lastUpdate: {$type: 'timestamp'}}});
+
+// Exercício 16 : Produza uma query onde os mutantes class omega ou gama passam a ter seu poder de 500 somente se seu poder for menor que 500 .
+
+use('class')
+db.xmen.updateMany({ $or:[ {class:/omega/i},{class:/gama/i} ] },
+  { $max: { power:500}, $currentDate: {lastUpdate: {$type: 'timestamp'}} }
+);
+//primeira tentativa abaixo. coloquei chaves demais. Consegui sem olhar o gabarito. HORAY
+// { $max: {power: 500},
+//       { $currentDate:{ lastUpdate:{ $type:'timestamp' } } } } )
+
+// Exercício 17 : Produza uma query onde os mutantes class gama passam a ter seu poder de 300 somente se seu poder for maior que 300 .
+use('class')
+db.xmen.updateMany( { class:/gama/i },
+  { $min: { power:300}, $currentDate: {lastUpdate: {$type: 'timestamp'}} }
+);
+
+// Exercício 18 : Decremente em 100 o poder dos mutantes que não possuem a propriedade class .
+use('class')
+db.xmen.updateMany({class: {$exists: false}},{ $inc:{power: -100}, $currentDate: {lastUpdate: {$type:'timestamp'}}});
+// db.xmen.updateMany({$exists: {class: false}},{ $inc:{power: -100}, $currentDate: {lastUpdate: {$type:'timestamp'}}}); minha primeira tentativa; troquei a posição da propriedade e do operador exists
+
+// Exercício 19 : Em apenas uma query adicione o campo areas com o seguinte array como valor: ["Students Room"] 
+// a todos os mutantes que são Senior Staff que tenham poder acima de 100 e para todos os Junior Staff com poder acima de 200.
+
+use('class')
+
+db.xmen.updateMany({$or:[{occupation:/senior staff/i, power:{$gt:100}},{occupation:/junior staff/i, power: {$gt:200}}]},{$set: {areas:['Students Room']}, $currentDate:{ lastUpdate:{$type:'timestamp'}}})
+
+// db.xmen.updateMany({$and:[{occupation:/senior staff/i},{power: {$gt:100}}], $and:[{occupation:/junior staff/i}, {power: {$gt:200}}]},{$set: {'areas':['Students Room']}, $currentDate:{ lastUpdate:{$type:'timestamp'}}}) PRIMEIRA TENTATIVA! trocar AND por OR apesar do exercício dizer 'E' . Dentro do Array , cada documento é  identificado por um objeto inteiro e todos os campos devem ser separados por vírgula - de acordo com o gabarito do Course.
+
+
+// Exercício 20 : Em apenas uma query, adicione o campo areas com ["Outside"] a todos os Junior Staff que não tenham o campo areas definido.
+use('class')
+
+db.xmen.updateMany({$and:[{areas: {$exists:false}, occupation:/junior staff/i}]},
+    {$set: { areas:['Outside']}, $currentDate:{ lastUpdate: {$type:'timestamp'}}})
+
+// Primeira tentativa abaixo. consegui corrigir reescrevendo a query sem olhar o gabarito. Horraayy! ;D 
+// db.xmen.updateMany(
+//   {$and:[{areas: {$exists:false}, occupation:/junior staff/i}]},
+//     { $set: {areas:['Outside'],
+//       $currentDate:{ lastUpdate: {$type:'timestamp'}}}})
+
+// As informações acima são sobre a Escola Jean Grey de Ensino Superior. Para todas as alterações realizadas, você deve sempre atualizar ou adicionar o campo lastUpdate , que armazena a data da última alteração com o tipo timestamp . Os exercícios devem ser realizados na ordem.
+
+// Exercício 14 : Remova o campo class dos documentos que possuem esse campo com o valor unknown .
+use('class')
 db.xmen.find({class: "unknown"});
 // Exercício 15 : Produza uma query que renomeie os campos de name para hero_name , e de true_name para full_name ; adicione o campo power com valor 100, em todos os documentos.
 // Exercício 16 : Produza uma query onde os mutantes class omega ou gama passam a ter seu poder de 500 somente se seu poder for menor que 500 .
