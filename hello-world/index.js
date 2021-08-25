38ee485cc8668e61b4e7a7ab6b5fb7f0cdbffe20
Lucas Lara <lucasml@live.com>
2021-08-25 16:27:27 -0300

    Course Examples

--- hello-world/index.js
+++
@@ -1,9 +0,0 @@
-const readLine = require('readline-sync');
-
-const nome = readLine.question('Qual seu nome? ')
-const idade = readLine.questionInt('Qual sua idade? ')
-
-console.log(`Olá, ${nome}! Você está com ${idade} anos nas costas!`)
-
-
-// console.log('Hello, world!');
