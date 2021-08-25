b8c023c9e85c2805d24ef67e6c5424e3c0e7422c
Lucas Lara <lucasml@live.com>
2021-08-24 21:34:41 -0300

    Testing asserts and fixing questionFloat at weight

--- my-scripts/imc.js
+++ my-scripts/imc.js
@@ -1,19 +1,32 @@
 const getData = require('readline-sync');
+const { equal } = require('assert');
+
+const weight = getData.questionFloat('Qual o seu peso? (Em Kg)');
+const height = getData.questionFloat('Qual o sua altura? (Em cm)');
+
+equal(weight, 80);
+equal(height, 1.8);
 
 const IMCCalculator = (weight, height) => {
-  
-  // const weight = getData.questionFloat('Qual o seu peso? (Em Kg)')
-  // const height = getData.questionInt('Qual o sua altura? (Em cm)')
-
-    const IMC = (weight / (height * height)).toFixed(2);
-    if( IMC <= 5) {
-      console.log(`Seu IMC é ${IMC}! Vc está abaixo do peso recomendado!`)
-    } else if (IMC > 5 && IMC <= 25){
-      console.log(`Seu IMC é ${IMC}! Seu peso Ta OK!`)
-    } else if (IMC > 25) {
-      console.log(`Seu IMC é ${IMC}! Vc está acima do peso!`)
-    }
-    return IMC;
+  console.log(`${height}, ${weight}`);
+  const IMC = (weight / (height * height));
+console.log(IMC);
+  if (IMC < 18.5) {
+    console.log(`Seu IMC é ${IMC}! Vc está abaixo do peso recomendado!`);
+  } else if (IMC >= 18.5 && IMC <= 24.9) {
+    console.log(`Seu IMC é ${IMC}! Seu peso Ta OK!`);
+  } else if (IMC >= 25 && IMC <= 29.9) {
+    console.log(`Seu IMC é ${IMC}! Vc está acima do peso!`);
+  } else if (IMC >= 30 && IMC <= 34.9) {
+    console.log(`Seu IMC é ${IMC}! Vc está com Obesidade Grau I!`);
+  } else if (IMC >= 35 && IMC <= 39.9) {
+    console.log(`Seu IMC é ${IMC}! Vc está com Obesidade Grau II!`);
+  } else if (IMC > 40) {
+    console.log(`Seu IMC é ${IMC}! Vc está com Obesidade Grau III e IV!`);
+  }else {
+    console.log('ta zuado o bagulho');
+  }
+  return IMC;
 };
 
-IMCCalculator(95, 1.80)
+IMCCalculator(weight, height);
