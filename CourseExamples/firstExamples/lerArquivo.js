083ae02857d139aace8871a710acd677789f0880
Lucas Lara <lucasml@live.com>
2021-08-26 16:12:47 -0300

    starting day

--- CourseExamples/firstExamples/lerArquivo.js
+++
@@ -1,9 +0,0 @@
-const { readFile } = require('fs');
-
-readFile('./arquivo.txt', (error, content) => {
-  if (error) {
-    console.error(`Erro ao ler o arquivo: ${error.message}`);
-    return;
-  }
-  console.log(`Arquivo lido com sucesso. Conteúdo: ${content.toString('utf8')}`);
-});
--- CourseExamples/io-local/writeFile.js
+++
@@ -1,7 +0,0 @@
-const { writeFile } = require('fs').promises;
-
-const nomeDoArquivo = 'meu-arquivo.txt';
-
-writeFile(nomeDoArquivo, 'Texto novo inserido com método Node "writeFile"')
-  .then(() => console.log('arquivo escrito com sucesso'))
-  .catch((e) => console.error(`Erro ao escrever o arquivo: ${e}`));
