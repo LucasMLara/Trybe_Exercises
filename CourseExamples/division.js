6a3a45b5172499fb9ee8b877876d1b1a1d24ba97
Lucas Lara <lucasml@live.com>
2021-08-25 22:33:49 -0300

    using lint

--- CourseExamples/division.js
+++ CourseExamples/division.js
@@ -1,5 +1,5 @@
 function dividirNumeros(num1, num2) {
-  if (num2 == 0) throw new Error("Não pode ser feito uma divisão por zero");
+  if (num2 === 0) throw new Error('Não pode ser feito uma divisão por zero');
 
   return num1 / num2;
 }
