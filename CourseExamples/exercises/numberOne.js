528253d9f9b5de8d6df02fa308afedf066a49da4
Lucas Lara <lucasml@live.com>
2021-08-26 15:10:59 -0300

    ggwp

--- CourseExamples/exercises/numberOne.js
+++ CourseExamples/exercises/numberOne.js
@@ -2,11 +2,20 @@ const firstFunction = (param1, param2, param3) => {
   const promise = new Promise((resolve, reject) => {
     if (typeof param1 !== 'number' || typeof param2 !== 'number' || typeof param3 !== 'number') reject(Error('Informe apenas números'));
     const result = ((param1 + param2) * param3);
+    if (result < 50) reject(Error('Valor muito baixo'));
     resolve(result);
   });
   return promise;
 };
 
+firstFunction(5, 10, 15)
+  .then((mathResult) => console.log(mathResult))
+  .catch(({ message }) => console.log(message));
+
 firstFunction(5, '10', 15)
+  .then((mathResult) => console.log(mathResult))
+  .catch(({ message }) => console.log(message));
+
+firstFunction(1, 1, 1)
   .then((mathResult) => console.log(mathResult))
   .catch(({ message }) => console.log(message));
