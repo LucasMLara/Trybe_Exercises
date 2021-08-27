f9163051aee46a1aa09b15f710ce077cc2b097bd
Lucas Lara <lucasml@live.com>
2021-08-27 16:46:15 -0300

    Saving first examples of the day

---
+++ CourseExamples/hello-express/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express();
+
+const recipes = [
+  {id:1 , name:'Lasanha', price: 40.0, waitTime:30},
+  {id:2 , name:'Macarrão a Bolonhesa', price: 35.0, waitTime:25},
+  {id:3 , name:'Macarrão com molho branco', price: 35.0, waitTime:25 },
+  {id:4 , name:'Macarrão com molho branco', price: 10.0, waitTime:25 }
+];
+
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/recipes/search', (req, res) => {
+  const { name, maxPrice, minPrice } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name) || r.price < parseInt(maxPrice) || r.price >= parseInt(minPrice));
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/drinks/search', (req, res) => {
+  const { name } = req.query;
+  const filteredRecipes = drinks.filter((r) => r.name.includes(name));
+  res.status(200).json(filteredRecipes);
+});
+
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
+
+
+
+
+// app.get('/recipes/:id', (req, res) => {
+//   const { id } = req.params;
+//   const recipe = recipes.find((r) => r.id === parseInt(id));
+//   // const recipe = recipes.find(({id}) => id === parseInt(id)); dessa forma não estava trocando os IDS ao trocar no navegador
+    
+//   if(!recipe) return res.status(404).json({message:'Recipe Not Found'})
+
+//   res.json(recipe);
+// });
+
+// app.get('/drinks', (req, res) => {
+//   res.json(drinks);
+// });
