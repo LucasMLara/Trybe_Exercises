1a69bc5c14879059bb01f8eda03e2be1c048566f
Lucas Lara <lucasml@live.com>
2021-10-26 18:41:28 -0300

    Refactor - SOLID - inversao de dependencia

--- socket/index.js
+++ socket/index.js
@@ -10,15 +10,9 @@ const corsOptions = {
 
 const io = require('socket.io')(http, corsOptions)
 
-io.on('connection', (socket) => {
-  socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo! :)')
-  
-  socket.on('ping', () => {
-    console.log(`${socket.id} emitiu um ping`);
-    io.emit('pong', `${socket.id} enviou um ping!`)
-    
-  })
-})
+app.use(express.static(__dirname + '/public'));
+
+require('./sockets/ping')(io)
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 http.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
