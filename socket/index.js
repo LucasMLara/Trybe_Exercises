7bd8bb25df33f7f489acfe937886a8bcf0dcfcc4
Lucas Lara <lucasml@live.com>
2021-10-26 18:26:12 -0300

    ping/pong back and front

--- socket/index.js
+++ socket/index.js
@@ -11,10 +11,11 @@ const corsOptions = {
 const io = require('socket.io')(http, corsOptions)
 
 io.on('connection', (socket) => {
-  console.log(`Usuário conectado. ID: ${socket.id}`)
+  socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo! :)')
   
   socket.on('ping', () => {
     console.log(`${socket.id} emitiu um ping`);
+    io.emit('pong', `${socket.id} enviou um ping!`)
     
   })
 })
