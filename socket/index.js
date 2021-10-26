77a0002d903f9274d8660a9809b5f123720fa8e5
Lucas Lara <lucasml@live.com>
2021-10-26 17:08:03 -0300

    IO instnace, OK

---
+++ socket/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const PORT = 3000
+
+const corsOptions = {
+  origin: `http://localhost:${PORT}`,
+  methods: ['GET', 'POST']
+}
+
+const io = require('socket.io')(http, corsOptions)
+
+io.on('connection', (socket) => console.log(`Usuário conectado. ID: ${socket.id}`))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+http.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
